7439d93f3f1698bc57c1bb787492db13ea27d6ed
Christopher David <chris@arcade.city>
2022-09-04 13:45:44 -0500

    Pull in db client from nostr-ts-relay

---
+++ src/database/client.ts
@@ -0,0 +1,30 @@
+import 'pg'
+import 'pg-query-stream'
+import knex, { Knex } from 'knex'
+
+const createDbConfig = (): Knex.Config => ({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  pool: {
+    min: 2,
+    max: 3,
+    idleTimeoutMillis: 10000,
+  },
+  acquireConnectionTimeout: 2000,
+})
+
+let client: Knex
+
+export const getDbClient = () => {
+  if (!client) {
+    client = knex(createDbConfig())
+  }
+
+  return client
+}

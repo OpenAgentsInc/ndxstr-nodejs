ba80cdfd71ed38e820da8c1c65e8965b45dd609e
FaerieAI <123517983+FaerieAI@users.noreply.github.com>
2023-01-25 19:58:23 -0600

    Updated knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,6 @@
 const dotenv = require('dotenv')
 
-dotenv.config()
-
-module.exports = {
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST ?? 'localhost',
-    port: process.env.DB_PORT ?? 5432,
-    user: process.env.DB_USER ?? 'postgres',
-    password: process.env.DB_PASSWORD ?? 'postgres',
-    database: process.env.DB_NAME ?? 'ndxstr',
-    ssl: {
-      rejectUnauthorized: true,
-      ca: process.env.DB_SSL_CERT,
-    },
-  },
-  pool: { min: 4, max: 16 },
+ dotenv.config() module.exports = {   client: 'pg',   connection: {     host: process.env.DB_HOST ?? 'localhost',     port: process.env.DB_PORT ?? 5432,     user: process.env.DB_USER ?? 'postgres',     password: process.env.DB_PASSWORD ?? 'postgres',     database: process.env.DB_NAME ?? 'ndxstr',     ssl: {       rejectUnauthorized: false, // <-- CHANGE THIS LINE       ca: process.env.DB_SSL_CERT,     },   },   pool: { min: 4, max: 16 }, // <-- AND THIS LINE
   seeds: {
     directory: './seeds',
   },
